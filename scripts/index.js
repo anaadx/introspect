f91617d3620c50c4c3a59632ad8547354aad58be
anaadx <anapaula.assisduarte.99@gmail.com>
2023-09-27 01:37:11 -0300

    feat: carousel base

---
+++ scripts/index.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+	$('.modal-trigger').on('click', function () {
+		$(this).closest('.card').find('.modal').fadeIn();
+	});
+
+	$('.modal').on('click', function (event) {
+		if ($(event.target).hasClass('modal')) {
+			$(this).closest('.card').find('.modal').fadeOut();
+		}
+	});
+
+	$('.closeImg').on('click', function () {
+		$(this).closest('.card').find('.modal').fadeOut();
+	});
+
+	$('.cases .carousel-projects').owlCarousel({
+		loop: false,
+		navSpeed: 500,
+		center: false,
+		rewind: true,
+		margin: 20,
+		responsiveClass: true,
+		nav: true,
+		autoplay: false,
+		autoWidth: false,
+		navContainerClass: 'navWhite owl-nav',
+		responsive: {
+			0: {
+				items: 1,
+			},
+			800: {
+				items: 1,
+			},
+			1024: {
+				items: 1,
+			},
+			1950: {
+				items: 1,
+			},
+		},
+	});
+});
